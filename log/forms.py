f332b8b83ba00157765c0e18ab8dba6a21f7723a
Naren <narenarya@live.com>
2016-08-03 11:37:24 +0530

    Update forms.py

--- log/forms.py
+++ log/forms.py
@@ -5,4 +5,4 @@ class LoginForm(AuthenticationForm):
     username = forms.CharField(label="Username", max_length=30, 
                                widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
     password = forms.CharField(label="Password", max_length=30, 
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'password'}))
+                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
